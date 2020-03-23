faa85d4fb1df299eb3941e7315473a9957a5b219
masaiqi <masaiqi.com@gmail.com>
2020-03-23 18:18:05 +0800

    打卡

---
+++ src/swordoffer/Q11.java
@@ -0,0 +1,44 @@
+package swordoffer;
+
+/**
+ * @author masaiqi
+ * @date 2020/3/23 6:15 下午
+ */
+public class Q11 {
+
+
+    /**
+     * 输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+     */
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(1)
+     */
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode head = null;
+        if (list1.val < list2.val) {
+            head = list1;
+            head.next = Merge(list1.next, list2);
+        } else {
+            head = list2;
+            head.next = Merge(list1, list2.next);
+        }
+        return head;
+    }
+}
