dfdeefa7b31b11215d5e3be30af6f872e9bacf71
masaiqi <masaiqi.com@gmail.com>
2020-03-24 22:36:49 +0800

    打卡

---
+++ src/swordoffer/Q12.java
@@ -0,0 +1,51 @@
+package swordoffer;
+
+/**
+ * @author masaiqi.com
+ * @date 2020/3/24
+ */
+class Q12 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+     */
+
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        return isSubtreeWithRoot(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+    }
+
+    private boolean isSubtreeWithRoot(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+        if (root1 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return isSubtreeWithRoot(root1.left, root2.left) && isSubtreeWithRoot(root1.right, root2.right);
+    }
+
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+}
